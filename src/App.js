9995d422e3c5a5c32f068b32158ae13572f22330
Jordan Ata <jordan.lsx13@gmail.com>
2019-06-30 14:06:06 +0300

    added error handling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import SpeechRecognition from 'react-speech-recognition';
 import axios from 'axios';
+import { Offline, Online } from 'react-detect-offline';
 import './App.css';
 
 const options = {
@@ -22,6 +23,7 @@ function App(props) {
 
   const [imgSrc, setImgSrc] = useState('');
   const [breedName, setBreedName] = useState('');
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const subBreeds = [
@@ -65,37 +67,81 @@ function App(props) {
 
       setBreedName(finalTranscript);
 
-      axios
-        .get(reqUrl)
-        .then(res => setImgSrc(res.data.message))
-        .then(resetTranscript());
+      const fetchData = async () => {
+        try {
+          const res = await axios(reqUrl);
+          setImgSrc(res.data.message);
+        } catch (err) {
+          setError(true);
+        }
+        resetTranscript();
+      };
+
+      fetchData();
     }
   }, [listening, finalTranscript, resetTranscript]);
 
-  if (!browserSupportsSpeechRecognition) {
-    alert('your browser does not support the Web Speech API');
-  }
-
   return (
-    <Container>
-      <Row className="justify-content-md-center">
-        <Col className="text-center">
-          <Button type="button" onClick={startListening}>
-            Listen
-          </Button>
-        </Col>
-      </Row>
-      <Row className="justify-content-md-center">
-        <Col className="text-center">
-          <h1>Show me a picture of a {breedName}</h1>
-        </Col>
-      </Row>
-      <Row>
-        <Col className="text-center">
-          {imgSrc ? <img alt="dog" src={imgSrc} /> : ''}
-        </Col>
-      </Row>
-    </Container>
+    <React.Fragment>
+      {browserSupportsSpeechRecognition ? (
+        <React.Fragment>
+          <Online>
+            <Container>
+              <Row className="justify-content-md-center">
+                <Col className="text-center">
+                  <Button
+                    size="lg"
+                    type="button"
+                    onClick={() => {
+                      setBreedName('');
+                      setError(false);
+                      setImgSrc('');
+                      startListening();
+                    }}
+                  >
+                    Say a breed
+                  </Button>
+                </Col>
+              </Row>
+              <Row className="justify-content-md-center">
+                <Col className="text-center">
+                  {error ? (
+                    <h1>
+                      The breed &lsquo;{breedName}&lsquo; could not be found.
+                    </h1>
+                  ) : (
+                    <h1>
+                      Show me a picture of a <br />
+                      {breedName}
+                    </h1>
+                  )}
+                </Col>
+              </Row>
+              <Row>
+                <Col className="text-center">
+                  {imgSrc ? <img alt={breedName} src={imgSrc} /> : ''}
+                </Col>
+              </Row>
+            </Container>
+          </Online>
+          <Offline>
+            <h1
+              style={{
+                fontSize: '72px',
+                color: '#dc3545',
+                textAlign: 'center'
+              }}
+            >
+              The app requires Internet access to work.
+            </h1>
+          </Offline>
+        </React.Fragment>
+      ) : (
+        <h1 style={{ fontSize: '72px', color: '#dc3545', textAlign: 'center' }}>
+          Your browser does not support the Web Speech API.
+        </h1>
+      )}
+    </React.Fragment>
   );
 }
 
